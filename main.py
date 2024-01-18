2055b17d0288ceb2dbf488b8b86a065e6cab4505
Prateek Thakur <prateekthakur272@gmail.com>
2024-01-18 18:31:37 +0530

    adds notes

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # Prateek Thakur 18/01/2024
 # imports
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 import uvicorn
 # mongodb connection
 from pymongo.mongo_client import MongoClient
@@ -9,7 +12,9 @@ from pymongo.server_api import ServerApi
 from config import mongo_password
 import certifi
 
-app = FastAPI(title='Notes Api', docs_url='/')
+app = FastAPI(title='Notes Api')
+app.mount('/static', StaticFiles(directory='static'), name='static')
+templates = Jinja2Templates('templates')
 
 uri = f"mongodb+srv://admin:{mongo_password}@notes.oibommt.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(uri,tlsCAFile=certifi.where())
@@ -18,6 +23,15 @@ try:
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
+    
+@app.get('/')
+def root(request:Request):
+    docs = client.notes.notes.find({})
+    notes = []
+    for doc in docs:
+        print(doc)
+        notes.append(doc)
+    return templates.TemplateResponse('index.html', {'request':request, 'notes':notes})
 
 if __name__ == '__main__':
     uvicorn.run('main:app', reload=True, port= 5000)
