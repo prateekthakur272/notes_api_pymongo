a5ca88e5e21cff5c18489cf370dc83137d35210c
Prateek Thakur <prateekthakur272@gmail.com>
2024-01-18 15:04:24 +0530

    connects mongo db

--- main.py
+++ main.py
@@ -2,8 +2,22 @@
 # imports
 from fastapi import FastAPI
 import uvicorn
+# mongodb connection
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+# env variables
+from config import mongo_password
+import certifi
 
 app = FastAPI(title='Notes Api', docs_url='/')
+
+uri = f"mongodb+srv://admin:{mongo_password}@notes.oibommt.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(uri,tlsCAFile=certifi.where())
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 
 if __name__ == '__main__':
     uvicorn.run('main:app', reload=True, port= 5000)
